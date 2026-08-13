2a8589b7bad04ee76c53a00bb69edf79f64ff638
William Allen <16820599+williamjallen@users.noreply.github.com>
2026-08-13 12:55:07 -0400

    Install eslint and prettier (#24)
    This commit adds `eslint` and `prettier` for JS and TS linting and
    formatting. Small adjustments may be needed as we fill out the UI, but
    this seems like a reasonable default for now.
    
    `typescript-eslint` doesn't support TypeScript 7 yet, so I have
    downgraded TS to version 6.

--- token_service/ui/src/index.ts
+++ token_service/ui/src/index.ts
@@ -2,6 +2,10 @@ import './styles/index.css';
 import m from 'mithril';
 import Hello from './views/Hello';
 
-const root = document.getElementById('app')!;
+const root = document.getElementById('app');
+
+if (root === null) {
+    throw new Error('Unable to find the application root element.');
+}
 
 m.mount(root, Hello);
