89a9a151dacba8a5e1f05b2be6ee4faa5d45722d
William Allen <16820599+williamjallen@users.noreply.github.com>
2026-08-31 14:35:12 -0600

    Enable Vite hot reloading (#40)
    The `npm run dev` shortcut currently enables Vite hot reloading, but the
    page cannot access the API under most browsers due to CORS restrictions.
    This PR enables Vite's proxy, allowing hot reloading to be used for all
    UI development.

--- token_service/ui/vite.config.js
+++ token_service/ui/vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import process from 'node:process';
+import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [
-      tailwindcss(),
-  ],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const port = process.env.TOKEN_SERVICE_PORT || env.TOKEN_SERVICE_PORT || 5000;
+
+  return {
+    plugins: [tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: `http://localhost:${port}`,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
