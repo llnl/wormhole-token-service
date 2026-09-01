87faf826895faa7620aa74e9d03ce6154b6ad33d
William Allen <16820599+williamjallen@users.noreply.github.com>
2026-09-01 11:30:51 -0600

    Add UI footer (#43)
    Adds a footer with a link to the GitHub repository and a copyright
    notice.

--- token_service/ui/src/views/Root.ts
+++ token_service/ui/src/views/Root.ts
@@ -1,11 +1,13 @@
 import m from 'mithril';
 import Header from './Header';
+import Footer from './Footer';
 
 const Root: m.Component = {
     view: (vnode: m.Vnode) =>
         m('div', { class: 'tw:min-h-screen tw:flex tw:flex-col' }, [
             m(Header),
             m('main', { class: 'tw:p-2 tw:flex-grow' }, vnode.children),
+            m(Footer),
         ]),
 };
 
